14edf674e07650fd8c91fb0dea04889afdef6bbd
6652410026ThayakornKoomphai <142290940+6652410026ThayakornKoomphai@users.noreply.github.com>
2023-09-19 20:58:45 +0700

    workshop10.py

---
+++ WorkshopA/workshop10.py
@@ -0,0 +1,23 @@
+def inputValue() :
+    number = int(input("ป้อนข้อความเลข 1-4 "))
+    return number
+
+number = inputValue()
+
+def numberToText() :
+    if number == 1 :
+        greet = "Welcome Freshman"
+    elif number == 2 :
+        greet = "Welcome Sophomore"
+    elif number == 3 :
+        greet = "Welcome Junior"
+    elif number == 4 :
+        greet = "Welcome Senior"    
+      else :
+        greet = "Invalid number"
+    return greet
+
+def showGreet():
+    print(numberToText())
+
+showGreet()
