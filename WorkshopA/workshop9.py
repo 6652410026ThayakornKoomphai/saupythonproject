84fe1a3a2344fede3cee2e6e9ae0ce7c41c04678
6652410026ThayakornKoomphai <142290940+6652410026ThayakornKoomphai@users.noreply.github.com>
2023-09-19 20:52:17 +0700

    workshop9.py

---
+++ WorkshopA/workshop9.py
@@ -0,0 +1,23 @@
+def EmpDetails() :
+    emp_ID = int(input("ใส่ ID พนักงาน: "))
+    emp_Name = input("ใส่ชื่อพนักงาน: ")
+    emp_Sales = float(input("ใส่จำนวนราคาที่พนักงานขายได้: "))
+    return emp_ID, emp_Name, emp_Sales
+  
+emp_ID, emp_Name, emp_Sales = EmpDetails()
+
+def commissionCheckAndCal() :
+    if emp_Sales < 1000 :
+        commission = 0
+    elif emp_Sales < 2001 :
+        commission = emp_Sales + (emp_Sales * 1 / 100)
+    elif emp_Sales < 3001 :
+        commission = emp_Sales + (emp_Sales * 3 / 100)
+    else :
+        commission = emp_Sales + (emp_Sales * 5 / 100)
+    return commission
+
+def showCommission() :
+    print(f"{empID} {empName} คุณได้ค่าคอมมิชชัน {commissionCheckAndCal()} บาท.")
+
+showCommission()
