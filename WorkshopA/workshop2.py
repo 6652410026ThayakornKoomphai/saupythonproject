edc617a87737f1082083df6410930a9e25265510
6652410026ThayakornKoomphai <142290940+6652410026ThayakornKoomphai@users.noreply.github.com>
2023-09-19 20:01:30 +0700

    workshop2.py

--- WorkshopA/workshop2.py
+++ WorkshopA/workshop2.py
@@ -1,7 +1,22 @@
-def inputPassStudentName():
-    password_student = (input('ป้อนชื่อรหัสนักเรียน:'))
-    name_student = (input('ป้อนชื่อนักเรียน:'))
-    return password_student, name_student
+def StudentDetails() :
+    stuID = int(input("ใส่ ID นักเรียน: "))
+    stuName = input("ใส่ชื่อนักเรียน: ")
+    stuResult_1 = float(input("ใส่คะแนนผลครั้งที่ 1: "))
+    stuResult_2 = float(input("ใส่คะแนนผลครั้งที่ 2: "))
+    stuResult_3 = float(input("ใส่คะแนนผลครั้งที่ 3: "))
+    return stuID, stuName, stuResult_1, stuResult_2, stuResult_3
+
+stuID, stuName, stuResult_1, stuResult_2, stuResult_3 = StudentDetails()
+
+def calAverageScore() :
+    return  (stuResult_1 + stuResult_2 + stuResult_3) / 3
+
+def showResult() :
+    print(f"{stuID} {stuName} you have average score {calAverageScore():.2f}")
+
+showResult()
+
+
 
 
 
