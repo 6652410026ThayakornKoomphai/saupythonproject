d0bd26f9bd8b618f4f681a88899f61664f5a8fe7
6652410026ThayakornKoomphai <142290940+6652410026ThayakornKoomphai@users.noreply.github.com>
2023-09-19 20:48:01 +0700

    workshop8.py

---
+++ WorkshopA/workshop8.py
@@ -0,0 +1,19 @@
+def Province():
+    province = input("กรอกจังหวัดของคุณ: ")
+    return province
+
+def PH():
+    ph = int(input("ใส่ค่า PH: "))
+    return ph
+
+def pH_Check() :
+    Province()
+    ph = PH()
+    if ph >= 7 and ph < 9 :
+        print("ผลลัพธ์เป็นปกติ")
+    elif ph > 8 :
+        print("ผลลัพธ์เป็นกรด")
+    else :
+        print("ผลลัพธ์เป็นอัลคาไลน์")
+
+phCheck()
