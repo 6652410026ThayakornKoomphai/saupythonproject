992e7d6b863f8b0836cb4828cca596cd112e0d83
6652410026ThayakornKoomphai <142290940+6652410026ThayakornKoomphai@users.noreply.github.com>
2023-09-19 20:09:17 +0700

    workshop3.py

--- WorkshopA/workshop3.py
+++ WorkshopA/workshop3.py
@@ -1,1 +1,15 @@
+def ProductDetails() :
+    productName = input("ใส่ชื่อสินค้า: ")
+    productPrice = float(input("ใส่ราคาสินค้า: "))
+    return productName, productPrice
 
+productName, productPrice = ProductDetails()
+
+def calculate():
+    tax = productPrice * 7 / 100
+    return tax
+
+def showCal() :
+    print(f"สินค้านี้มีภาษี {calculate()} บาท")
+
+showCal()
